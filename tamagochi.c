32ba83d8eb409f44d9c739bde43711e0dd89732f
fernanzp <fperez23@ucol.mx>
2023-10-21 13:42:35 -0600

    Add the menu

--- tamagochi.c
+++ tamagochi.c
@@ -6,6 +6,41 @@
 
 int main()
 {
-	
+	//Declaramos las variables
+	int opc;
+
+	for(int i = 1; i == 1;)
+	{
+		printf("\n\n			  --Tamagochi--\n");
+		printf("			Jugar.........[1]");
+		printf("\n			Comer.........[2]");
+		printf("\n			Dormir........[3]");
+		printf("\n			Ver estado....[4]");
+		printf("\n			Salir.........[5]");
+		printf("\n			");
+		scanf("%d", &opc);
+
+		switch(opc)
+		{
+			case 1:
+				
+				break;
+			case 2:
+
+				break;
+			case 3:
+
+				break;
+			case 4:
+
+				break;
+			case 5:
+				return 0;
+				break;
+			default:
+				printf("\n			Error, opción inexistente!\n");
+				return 1;
+		}
+	}
 	return 0;
 }
