5c58ceb8da7d9b90d134a75faf019e09c0861666
fernanzp <fperez23@ucol.mx>
2023-10-21 15:20:21 -0600

    Add the funcion jugar UNO

--- tamagochi.c
+++ tamagochi.c
@@ -23,26 +23,44 @@ int main()
 		switch(opc)
 		{
 			case 1:
-				int opc_jugar;
-				printf("\n\n			--Jugar--\n");
-				printf("			Jugar a la pelota...[1]");
-				printf("\n			Jugar videojuegos...[2]");
-				scanf("%d", &opc_jugar);
-
-				switch(opc_jugar)
+				for(int i = 1; i == 1;)
 				{
-					case 1:
-						printf("\n			Gracias por jugar a la pelota conmigo :D\n");
-						energia -= 10;
-						hambre += 5;
-						break;
-					case 2:
-						printf("\n			Gracias por jugar videojuegos conmigo :D\n");
-						energia -= 5;
-						hambre += 10;
-						break;
-
+					int opc_jugar;
+					printf("\n\n			       --Jugar--\n");
+					printf("			Jugar a la pelota...[1]");
+					printf("\n			Jugar videojuegos...[2]");
+					printf("\n			Jugar UNO...........[3]");
+					printf("\n			Regresar............[4]");
+					printf("\n			");
+					scanf("%d", &opc_jugar);
+	
+					switch(opc_jugar)
+					{
+						case 1:
+							printf("\n			Gracias por jugar a la pelota conmigo :D\n");
+							energia -= 10;
+							hambre += 5;
+							break;
+						case 2:
+							printf("\n			Gracias por jugar videojuegos conmigo :D\n");
+							energia -= 5;
+							hambre += 10;
+							break;
+						case 3:
+							printf("\n			Gracias por jugar UNO conmigo :D\n");
+							energia -= 5;
+							hambre += 5;
+							break;
+						case 4:
+							printf("\n			¡Gracias por jugar con tu Tamagochi!\n");
+							i++;
+							break;
+						default:
+							printf("\n			¡Error, opción inexistente!\n");
+							break;
+					}
 				}
+
 				break;
 			case 2:
 
